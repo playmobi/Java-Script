b84bbb208096d737bfa246ac8d2d67d1abcc063c
Charly B <31864082+playmobi@users.noreply.github.com>
2021-05-29 15:25:48 -0500

    Add files via upload

---
+++ sesion_2/ejercicio_1/ejecicio1.js
@@ -0,0 +1,73 @@
+// var time = 13;
+// var saludo;
+
+// if (time < 12) {
+//     saludo = "Buenos dias";
+// }
+
+// if (time < 20) {
+//     saludo = "Buenas tardes";
+// }
+
+// if (time >= 20) {
+//     saludo = "Buenas noches";
+// }
+
+// console.log (saludo)
+
+
+// var time = 24;
+// var saludo;
+
+// if (time >= 0 && time < 12) {
+//     saludo = "Buenos días";
+// } else if (time >= 12 && time < 20){
+//     saludo = "Buenas tardes";
+// } else if (time >= 20 && time <= 24){
+//     saludo = "Buenas noches";
+// } else {
+//     saludo= "Hora desconocida";
+// }
+
+// console.log(saludo)
+
+
+var day = 5;
+var text;
+
+switch (day) {  
+    case 0:    
+    text = "Sunday";    
+    break;
+
+    case 1:    
+    text = "Monday";    
+    break; 
+    
+    case 2:    
+    text = "Tuesday";    
+    break;  
+    
+    case 3:    
+    text = "Wednesday";    
+    break;  
+
+    case 4:   
+    text = "Thursday";   
+    break;  
+
+    case 5:    
+    text = "Friday";   
+    break;
+
+    case 6:   
+    text = "Saturday";    
+    break;  
+    default:    
+    text = "Error";
+
+    }
+
+    console.log(text);
+
+    
